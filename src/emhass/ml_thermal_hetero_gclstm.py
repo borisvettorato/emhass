ee3783ce3bf05b922b9a482df042e495d609bb26
borisvettorato <92640277+borisvettorato@users.noreply.github.com>
2026-03-15 12:09:03 +0100

    Add HeteroGCLSTM model implementation for thermal predictions

---
+++ src/emhass/ml_thermal_hetero_gclstm.py
@@ -0,0 +1,14 @@
+class HeteroGCLSTM:
+    def __init__(self, input_size, hidden_size, num_layers):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # Further initialization code here
+
+    def forward(self, x):
+        # Forward pass implementation here
+        pass
+
+    def predict(self, x):
+        # Prediction implementation here
+        return self.forward(x)

c62c55bff804e25ad201ce36d0250be77de6d755
borisvettorato <92640277+borisvettorato@users.noreply.github.com>
2026-03-15 12:20:05 +0100

    Add HeteroGCLSTM class for training model

--- src/emhass/ml_thermal_hetero_gclstm.py
+++ src/emhass/ml_thermal_hetero_gclstm.py
@@ -12,3 +12,17 @@ class HeteroGCLSTM:
     def predict(self, x):
         # Prediction implementation here
         return self.forward(x)
+
+class HeteroGCLSTM:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.wrapper = ThermalModelWrapper(input_size, hidden_size, output_size)
+
+    def train(self, data_loader, criterion, optimizer):
+        self.wrapper.model.train()
+        for inputs, targets in data_loader:
+            optimizer.zero_grad()
+            outputs = self.wrapper.forward(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+        return loss.item()
